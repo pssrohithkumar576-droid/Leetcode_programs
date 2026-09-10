ca344c462adde872eab5fb8e6299328665094e45
Peruri Surya Satya Rohith Kumar <pssrohithkumar576@gmail.com>
2026-09-10 11:45:50 +0530

    Time: 76 ms (88.79%) | Memory: 31.3 MB (63.35%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -13,7 +13,7 @@ class MinStack:
 
     def pop(self) -> None:
         self.mnStack.pop()
-        return self.st.pop()
+        self.st.pop()
 
     def top(self) -> int:
         return self.st[-1]
